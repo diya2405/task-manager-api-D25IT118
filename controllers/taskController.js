40c3ee9d852d514eb427600cda831cfee1da4713
diya2405 <diyanamya@gmail.com>
2026-08-23 13:23:11 +0530

    Add user scoping to Task model & CRUD operations - associate each task with user ID and isolate task lists per user

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -2,7 +2,7 @@ const Task = require('../models/Task');
 
 exports.getAllTasks = async (req, res, next) => {
   try {
-    const tasks = await Task.find();
+    const tasks = await Task.find({ user: req.user.id }).sort({ createdAt: -1 });
     res.status(200).json(tasks);
   } catch (err) {
     next(err);
@@ -11,7 +11,7 @@ exports.getAllTasks = async (req, res, next) => {
 
 exports.getTaskById = async (req, res, next) => {
   try {
-    const task = await Task.findById(req.params.id);
+    const task = await Task.findOne({ _id: req.params.id, user: req.user.id });
     if (!task) return res.status(404).json({ error: 'Task not found' });
     res.status(200).json(task);
   } catch (err) {
@@ -22,7 +22,10 @@ exports.getTaskById = async (req, res, next) => {
 
 exports.createTask = async (req, res, next) => {
   try {
-    const task = await Task.create(req.body);
+    const task = await Task.create({
+      ...req.body,
+      user: req.user.id,
+    });
     res.status(201).json(task);
   } catch (err) {
     next(err);
@@ -31,10 +34,14 @@ exports.createTask = async (req, res, next) => {
 
 exports.updateTask = async (req, res, next) => {
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const task = await Task.findOneAndUpdate(
+      { _id: req.params.id, user: req.user.id },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
     if (!task) return res.status(404).json({ error: 'Task not found' });
     res.status(200).json(task);
   } catch (err) {
@@ -45,7 +52,7 @@ exports.updateTask = async (req, res, next) => {
 
 exports.deleteTask = async (req, res, next) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOneAndDelete({ _id: req.params.id, user: req.user.id });
     if (!task) return res.status(404).json({ error: 'Task not found' });
     res.status(200).json({ message: 'Task deleted', task });
   } catch (err) {
--- models/Task.js
+++ models/Task.js
@@ -4,6 +4,11 @@ const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String },
   completed: { type: Boolean, default: false },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
